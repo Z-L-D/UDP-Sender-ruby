06d1b1375cefe47d1901d91ded6e8327001e9a92
Z-L-D <zacldoyle@gmail.com>
2022-06-13 17:46:35 -0500

    -

--- program.rb
+++ program.rb
@@ -1,5 +1,9 @@
 require 'socket'
 
-s1 = UDPSocket.new
-s1.bind("127.0.0.1", 0)
-s1.send "aaa", 0, "127.0.0.1", 5500
+command = "Hello from Ruby"
+address = "127.0.0.1"
+port = 5500
+
+socket_object = UDPSocket.new
+socket_object.bind("127.0.0.1", 0)
+socket_object.send command, 0, address, port
